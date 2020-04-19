438d543070b4f235325be79ff8d8c487db34b2b3
agustin-SATELLITE <normandagustin@gmail.com>
2020-04-19 15:27:33 -0300

    Commit

---
+++ TrabajosPracticos/Unidad2/NoSql/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO RUBRO(CodRub,NomRub) VALUES (1,'RubUno');
+INSERT INTO RUBRO(CodRub,NomRub) VALUES (2,'RubDos');
+INSERT INTO RUBRO(CodRub,NomRub) VALUES (3,'RubTres');
+INSERT INTO RUBRO(CodRub,NomRub) VALUES (4,'RubCuatro');
+
+INSERT INTO PRODUCTO(CodRub,CodPro,NomPro) VALUES (1,1,'ProdUnoUno');
+INSERT INTO PRODUCTO(CodRub,CodPro,NomPro) VALUES (2,1,'ProdDosUno');
+INSERT INTO PRODUCTO(CodRub,CodPro,NomPro) VALUES (2,2,'ProdDosDos');
+INSERT INTO PRODUCTO(CodRub,CodPro,NomPro) VALUES (3,1,'ProdTresUno');
